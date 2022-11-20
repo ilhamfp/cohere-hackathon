5e2d65c4eff61ae5ec2a4023dd8ef299929058ed
Ilham Firdausi Putra <ilham.putra@coinbase.com>
2022-11-20 16:20:59 +0800

    introduce debug mode

--- api/generate.py
+++ api/generate.py
@@ -1,6 +1,7 @@
 import cohere
 import os
 
+IS_DEBUG_MODE = False
 API_KEY = os.getenv("COHERE_API_KEY")
 co = cohere.Client(API_KEY)
 
@@ -42,7 +43,10 @@ class LovedOnes:
     def update_latest_chat_loved_ones(self, loved_ones_text) -> None:
         trimmed_text = loved_ones_text[:loved_ones_text.find("{}:".format(self.user_name))].strip()
         trimmed_text = trimmed_text.replace("--", "").strip()
-        print("Trimmed text: ", trimmed_text)
+
+        if IS_DEBUG_MODE:
+            print("Trimmed text: ", trimmed_text)
+            
         self.latest_chat[-1].loved_ones_text = trimmed_text
 
     def generate_prompt(self) -> str:
@@ -75,36 +79,16 @@ class LovedOnes:
             cur_messages = self.latest_chat[messages_idx].to_str(self.user_name, self.loved_ones_name)
             cur_messages_length = len(cur_messages.split(" ")) + 2
 
-        print("Total token length: ", token_count)
+        if IS_DEBUG_MODE:
+            print("Total prompt token length: ", token_count)
+
         return initial_prompt + messages_prompt
-
-def generate_example():
-    lo = LovedOnes("Ilham", "Sally", "30 years old human being")
-    lo.update_latest_chat_user("Hello how are you doing?")
-    lo.update_latest_chat_loved_ones("I am doing good. How about you?")
-
-    lo.update_latest_chat_user("I am doing good too! Where are you yesterday?")
-    prompt = lo.generate_prompt()
-    print("prompt: ", prompt)
-
-    response = co.generate(  
-        model='xlarge',  
-        prompt = prompt,  
-        max_tokens=40,  
-        temperature=0.6,  
-        stop_sequences=["--"])
-
-    print("response: ", response)
-    startup_idea = response.generations[0].text
-    print("Sally: ", startup_idea)
 
 user_loved_ones = {}
 
 
 if __name__ == "__main__":
     print("Welcome to Sally bot!")
-    # generate_example()
-
     print("What's your name?")
     user_name = input("Your name: ")
     lo = LovedOnes(user_name)
@@ -121,9 +105,11 @@ if __name__ == "__main__":
         msg = input("Input you message: ")
         lo.update_latest_chat_user(str(msg).strip())
         prompt = lo.generate_prompt()
-        print("------------------------------------------")
-        print("prompt: ", prompt)
-        print("------------------------------------------")
+
+        if IS_DEBUG_MODE:
+            print("------------------------------------------")
+            print("prompt: ", prompt)
+            print("------------------------------------------")
 
         response = co.generate(  
             model='xlarge',  
@@ -132,7 +118,12 @@ if __name__ == "__main__":
             temperature=0.6,  
             stop_sequences=["{}:".format(lo.user_name)])
 
-        # print("response: ", response)
         resp_msg = response.generations[0].text
-        print("{}: {}".format(lo.loved_ones_name, resp_msg))
+
+        if IS_DEBUG_MODE:
+            print("------------------------------------------")
+            print("resp_msg: ", resp_msg)
+            print("------------------------------------------")
+
         lo.update_latest_chat_loved_ones(resp_msg)
+        print("{}: {}".format(lo.loved_ones_name, lo.latest_chat[-1].loved_ones_text))
